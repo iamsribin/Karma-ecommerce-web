1ea459d5c2626e464812cae6191ea18e5117531e
Sribin <sribin85@gmail.com>
2024-07-25 08:20:19 -0400

    remove uploads form gitgnore

--- connetion/mongo-connetion.js
+++ connetion/mongo-connetion.js
@@ -3,8 +3,7 @@ const Coupon = require("../models/adminModels/coupon");
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL, {
-    });
+    await mongoose.connect(process.env.DB_URL);
     
     await Coupon.syncIndexes();
     console.log('Database connected');
